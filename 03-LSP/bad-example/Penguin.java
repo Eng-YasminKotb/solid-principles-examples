c2b89241afa6ed5ba70a7da87941679fa607b286
Eng-YasminKotb <122429943+Eng-YasminKotb@users.noreply.github.com>
2025-11-17 22:26:29 +0200

    Add files via upload

---
+++ 03-LSP/bad-example/Penguin.java
@@ -0,0 +1,8 @@
+package badExample;
+
+public class Penguin extends Bird{
+    @Override
+    public void fly(){
+        throw new UnsupportedOperationException("Penguin can't fly");
+    }
+}
---
+++ 03-LSP/bad-example/Sparrrow.java
@@ -0,0 +1,8 @@
+package badExample;
+
+public class Sparrrow extends Bird{
+    @Override
+    public void fly(){
+        System.out.println("Sparrow is flying");
+    }
+}
