cbd3875123487d3fac2523d293836923f7de3cc7
Eng-YasminKotb <122429943+Eng-YasminKotb@users.noreply.github.com>
2025-11-17 22:12:00 +0200

    Delete 01-SRP directory

--- 01-SRP/EmailService.java
+++
@@ -1,5 +0,0 @@
-public class EmailService {
-    public void sendWelcomeEmail(User user) {
-        System.out.println("Sending welcome email to " + user.getEmail());
-    }
-}
--- 01-SRP/UserRepository.java
+++
@@ -1,5 +0,0 @@
-public class UserRepository {
-    public void save(User user) {
-        System.out.println("Saving user to DB: " + user.getName());
-    }
-}
